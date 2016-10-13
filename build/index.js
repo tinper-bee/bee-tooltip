d5fa97e6c9e8acf6547f0dbffcf40c7402e28582
ahua52 <1468492018@qq.com>
2016-10-13 13:43:46 +0800

    ver. 0.0.4

---
+++ build/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+exports.__esModule = true;
+exports.Tooltip = undefined;
+
+var _Tooltip2 = require('./Tooltip');
+
+var _Tooltip3 = _interopRequireDefault(_Tooltip2);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+exports.Tooltip = _Tooltip3["default"];
